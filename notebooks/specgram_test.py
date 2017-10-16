390842b0d5440675da81ad74856f556b7cd2369e
Nicolas Cuervo <nicolas.cuervo@ettus.com>
2017-10-16 20:28:24 +0200

    wip

--- notebooks/specgram_test.py
+++ notebooks/specgram_test.py
@@ -1,61 +1,86 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import scipy as sp
+
+""" Generates Spectrograms based on Raw data recorded with GNURadio """
+
+import os
+
 import numpy as np
+import scipy as sp
 from scipy import signal
 from scipy.misc import imsave
-
 # Set up constants and data
-scn = 0
-NFFT = 64 # Length of window segments to later avg. avg over 120FFTs = 0.768ms
+SCN = 2
+SNR = 5
+# While using sp.signal.specgram there are two fields that regard the FFT size:
+# * nfft:    Length of the FFT used, if a zero padded FFT is desired. If None,
+#            the  FFT length is nperseg. Defaults to None.
+# * nperseg: Length of each segment. Defaults to None, but if window is str or
+#            tuple, is set to 256, and if window is array_like, is set to the
+#            length of the window.
+# Length of window segments to later avg. avg over 120FFTs = 0.768ms
+NFFT = 64
+NUM_TRAIN_IMG = 2000
 # TODO: change NFFT name?
 # 5e5 samples for 50ms time window
-count = int(5e5)
-# gain_path = 'gain_0_0'
-snr = 5
-dir_path = os.path.join('..', '..', 'data', 'final_pu', 'with_dc')
-plt.gray()
-train_path = os.path.join(dir_path, '..', '..', 'pic_set', 'SNR_{}'.format(snr), 'train')
-test_path = os.path.join(dir_path, '..', '..', 'pic_set', 'SNR_{}'.format(snr), 'test')
-print("Loading data...")
+COUNT = int(5e5) # TODO do I use this?
+DIR_PATH = os.path.join('..', '..', 'data', 'final_pu', 'with_dc')
+# plt.gray()
+TRAIN_PATH = os.path.join(DIR_PATH, '..', '..', 'pic_set', 'SNR_{}'.format(SNR), 'train')
+TEST_PATH = os.path.join(DIR_PATH, '..', '..', 'pic_set', 'SNR_{}'.format(SNR), 'test')
 # Count = 1.25 e 9 samples for 2500 pictures back to back
 # + 500 samples for overflow avoidance (not expected to use them)
-print("Loading DONE")
+
+# TODO: I need to check first if the file to be analyzed exists, otherwise
+# this is pointless
 
 # Check if the dirs for the images exists. If not, create
 ## Checking for train dir
 print("Checking for directories...")
-if not os.path.exists(train_path):
-    print("Creating directory at ", os.path.realpath(train_path))
-    os.makedirs(train_path)
+if not os.path.exists(TRAIN_PATH):
+    print("Creating directory at ", os.path.realpath(TRAIN_PATH))
+    os.makedirs(TRAIN_PATH)
 ## Checking for test dir
-if not os.path.exists(test_path):
-    print("Creating directory at ", os.path.realpath(test_path))
-    os.makedirs(test_path)
+if not os.path.exists(TEST_PATH):
+    print("Creating directory at ", os.path.realpath(TEST_PATH))
+    os.makedirs(TEST_PATH)
 ## Checking for class scenario directory
 ### Train
-train_scn_path = os.path.join(train_path, 'scn_{}'.format(scn))
-if not os.path.exists(train_scn_path):
-    print("Creating directory at ", os.path.realpath(train_scn_path))
-    os.makedirs(train_scn_path)
+TRAIN_SCN_PATH = os.path.join(TRAIN_PATH, 'scn_{}'.format(SCN))
+if not os.path.exists(TRAIN_SCN_PATH):
+    print("Creating directory at ", os.path.realpath(TRAIN_SCN_PATH))
+    os.makedirs(TRAIN_SCN_PATH)
 ### Test
-test_scn_path = os.path.join(test_path, 'scn_{}'.format(scn))
-if not os.path.exists(test_scn_path):
-    print("Creating directory at ", os.path.realpath(test_scn_path))
-    os.makedirs(test_scn_path)
+TEST_SCN_PATH = os.path.join(TEST_PATH, 'scn_{}'.format(SCN))
+if not os.path.exists(TEST_SCN_PATH):
+    print("Creating directory at ", os.path.realpath(TEST_SCN_PATH))
+    os.makedirs(TEST_SCN_PATH)
 
-af = open(os.path.join(dir_path, 'scn_{}_snr_{}'.format(scn, snr)), 'rb')
+AF = open(os.path.join(DIR_PATH, 'scn_{}_snr_{}'.format(SCN, SNR)), 'rb')
 
-for i in range(64):
-    af.seek(7700*i)
-    data = sp.fromfile(af, dtype=sp.complex64, count = (7700))
-    f, t, Sxx = signal.spectrogram(data, fs=10e6, mode='magnitude', return_onesided=False, nperseg=NFFT, noverlap=0)
-    avgd = np.average(Sxx, axis=1)
-    if i == 0:
-        stacked = np.array(avgd)
+for j in range(1000):
+    for i in range(64):
+        # af.seek(7700*i) # every sample has 4 bytes THIS ONE IS TOTALLY WRONG!!!
+        # af.seek(7700*8*i, 0) # every sample has 4 bytes I DUNNO WHY THIS IS NOT THE SAME AS BELOW
+        AF.seek(7700*8, 1) # every sample has 4 bytes THIS ONE WORKS
+        data = sp.fromfile(AF, dtype=sp.complex64, count=7700)
+        # f, t, Sxx = signal.spectrogram(data, fs=10e6, mode='psd', return_onesided=False, nperseg=NFFT, noverlap=0)
+        _, _, Sxx = signal.spectrogram(data,
+                                       fs=10e6,
+                                       mode='psd',
+                                       return_onesided=False,
+                                       nperseg=NFFT,
+                                       noverlap=0)
+        Sxx = sp.fftpack.fftshift(Sxx, axes=0)
+        avgd = np.average(Sxx, axis=1)
+        if i == 0:
+            stacked = np.array(avgd)
+        else:
+            stacked = np.vstack([stacked, avgd])
+    if i < NUM_TRAIN_IMG:
+        imsave(os.path.join(TRAIN_SCN_PATH, 'image_{}.jpg'.format(j)), stacked)
     else:
-        stacked = np.vstack([stacked, avgd])
-af.close()
-stacked_scaled = stacked * 255.0 / np.amax(stacked)
-print("avgd_scaled shape: ", stacked_scaled.shape)
-imsave('what.jpg', stacked_scaled)
+        imsave(os.path.join(TEST_SCN_PATH, 'image_{}.jpg'.format(j)), stacked)
+
+
+AF.close()
